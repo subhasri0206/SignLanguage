7186f602b01cade151dfbde0e5ccd350f1fd49ae
Subhasri K <124023729+subhasri0206@users.noreply.github.com>
2025-02-01 10:18:18 +0530

    updated files

---
+++ speech_to_text.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+def speech_to_text():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        try:
+            audio = r.listen(source)
+            text = r.recognize_google(audio)
+            return text
+        except sr.UnknownValueError:
+            return "Could not understand audio"
+        except sr.RequestError as e:
+            return f"Error: {e}"
