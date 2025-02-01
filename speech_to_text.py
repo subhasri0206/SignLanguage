6266df4e40d5fbca5500ab4562b0b09f9c82d409
Subhasri K <124023729+subhasri0206@users.noreply.github.com>
2025-02-01 10:25:31 +0530

    updated files

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,13 +1,25 @@
+import tkinter as tk
 import speech_recognition as sr
-
-def speech_to_text():
+from tkinter import messagebox
+def Speech_to_Text():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         try:
             audio = r.listen(source)
             text = r.recognize_google(audio)
-            return text
+            txtSpeech.insert(tk.END, text + "\n")
+
         except sr.UnknownValueError:
-            return "Could not understand audio"
+            txtSpeech.insert(tk.END, "Could not understand audio\n")
         except sr.RequestError as e:
-            return f"Error: {e}"
+            txtSpeech.insert(tk.END, "Error: {0}\n".format(e))
+
+            
+def reset_txtSpeech():
+    txtSpeech.delete("1.0", tk.END)   
+
+def exit_system():
+    result = messagebox.askyesno('Speech to Text', 'Do you want to exit?')
+    if result == True:
+        root.destroy()
+        return
